0ab2c1400e9e29688a04aaa3f372124b55e00c48
stef4k <stef4k@gmail.com>
2021-04-22 02:26:55 +0300

    Add Suite class for MyMath tests

---
+++ unittesting/src/test/java/math/MyMathTestSuite.java
@@ -0,0 +1,13 @@
+package math;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ MyMathTest.class, MyMathTest_factorial_parameterized.class })
+public class MyMathTestSuite {
+	/*
+	 * This class remains empty. Is used only as a holder of the above annotations.
+	 */
+
+}
