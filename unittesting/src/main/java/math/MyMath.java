5f9afbac6a14688927c38b065f7d750f2f318d35
stef4k <stef4k@gmail.com>
2021-04-22 18:03:41 +0300

    Add explanatory comments in MyMath class

--- unittesting/src/main/java/math/MyMath.java
+++ unittesting/src/main/java/math/MyMath.java
@@ -19,12 +19,13 @@ public class MyMath {
 	 *                                     12
 	 */
 	public int factorial(int n) {
+		// Different Exception messages for negative and high positive numbers
 		if (n < 0) {
 			throw new IllegalArgumentException("Cannot find factorial for negative number");
 		} else if (n > 12) {
 			throw new IllegalArgumentException("Cannot find factorial for high positive number");
 		}
-		
+		// Calculating the factorial
 		int fact = 1;
 		for (int i = 1; i <= n; i++) {
 			fact = fact * i;
