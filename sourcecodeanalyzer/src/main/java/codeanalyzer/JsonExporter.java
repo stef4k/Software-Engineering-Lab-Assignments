a5592f2cfbd439f997f71e4ee3dc1235abd4826e
stef4k <stef4k@gmail.com>
2021-06-02 22:42:09 +0300

    Fix json exporter name

---
+++ sourcecodeanalyzer/src/main/java/codeanalyzer/JsonExporter.java
@@ -0,0 +1,10 @@
+package codeanalyzer;
+
+import java.util.Map;
+
+public class JsonExporter implements MetricsExporter{
+	@Override
+	public void writeFile(Map<String, Integer> metrics, String filepath) {
+		// Functionality not implemented yet
+	}
+}
